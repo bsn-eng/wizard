4d63581f1444891c779dfa6108a8af3c3260cb05
ManulParihar <manul@blockswap.network>
2022-11-04 00:15:11 +0530

    Added _getStakehouseTicker function

--- logic/utils.js
+++ logic/utils.js
@@ -1,12 +1,20 @@
 const { getContractInstance } = require('./contracts');
 
-const _getDAOAddressOfLSDN = async (signer, liquidStakingManagerAddress) => {
+const _getDAOAddress = async (signer, liquidStakingManagerAddress) => {
 
     const contract = (await getContractInstance(signer)).liquidStakingManager(liquidStakingManagerAddress);
 
     return contract.dao();
 };
 
+const _getStakehouseTicker = async (signer, liquidStakingManagerAddress) => {
+
+    const contract = (await getContractInstance(signer)).liquidStakingManager(liquidStakingManagerAddress);
+
+    return contract.stakehouseTicker();
+};
+
 module.exports = {
-    _getDAOAddressOfLSDN
+    _getDAOAddress,
+    _getStakehouseTicker
 }
