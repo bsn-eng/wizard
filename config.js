f258cb0cf377ece04929a8d091013eabdc39b94d
ManulParihar <manul@blockswap.network>
2022-11-04 19:29:07 +0530

    Added and exposed SavETHPoolSubPackage in the SDK and added few functions

--- config.js
+++ config.js
@@ -2,15 +2,19 @@ const { DeployerSubPackage } = require('./interface/deployerClass');
 const { ConstantsSubPackage } = require('./interface/constantsClass');
 const { ContractSubPackage } = require('./interface/contractClass');
 const { UtilsSubPackage } = require('./interface/utilsClass');
+const { SavETHPoolSubPackage } = require('./interface/savETHPoolClass');
 
 class Wizard {
 
-    constructor({signer, liquidStakingManagerAddress=null}) {
+    constructor({
+        signer, 
+        liquidStakingManagerAddress=null, 
+        savETHPoolAddress=null
+    }) {
         
         this.deployer = new DeployerSubPackage(signer);
         this.utils = new UtilsSubPackage(signer, liquidStakingManagerAddress);
-        // this.pool = new PoolSubPackage(signer);
-        // this.giantPool = new GiantPoolSubPackage(signer);
+        this.savETHPool = new SavETHPoolSubPackage(signer, savETHPoolAddress);
         
         this.contractInstance = new ContractSubPackage(signer);
         this.constants = new ConstantsSubPackage(signer);
---
+++ interface/savETHPoolClass.js
@@ -0,0 +1,7 @@
+class SavETHPoolSubPackage {
+
+}
+
+module.exports = {
+    SavETHPoolSubPackage
+};
