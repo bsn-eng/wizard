e96f0e4533a83bc76ebb08b50113629941d1d995
ManulParihar <manul@blockswap.network>
2022-11-04 20:09:03 +0530

    Added _updateAccumulatedETHPerLP function

--- logic/feesAndMevPool.js
+++ logic/feesAndMevPool.js
@@ -9,6 +9,14 @@ const _totalShares = async (signer, feesAndMevPoolAddress) => {
     return contract.totalShares();
 };
 
+const _updateAccumulatedETHPerLP = async (signer, feesAndMevPoolAddress) => {
+
+    const contract = (await getContractInstance(signer)).feesAndMevPool(feesAndMevPoolAddress);
+
+    return contract.updateAccumulatedETHPerLP();
+};
+
 module.exports = {
-    _totalShares
+    _totalShares,
+    _updateAccumulatedETHPerLP
 };
