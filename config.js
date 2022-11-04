b3487a8784b0035d7aa4d5d0fbdb57bcadd48540
ManulParihar <manul@blockswap.network>
2022-11-04 17:40:15 +0530

    Exposed UtilsSubPackage in the SDK

--- config.js
+++ config.js
@@ -1,12 +1,14 @@
 const { DeployerSubPackage } = require('./interface/deployerClass');
 const { ConstantsSubPackage } = require('./interface/constantsClass');
 const { ContractSubPackage } = require('./interface/contractClass');
+const { UtilsSubPackage } = require('./interface/utilsClass');
 
 class Wizard {
 
     constructor(signer) {
         
         this.deployer = new DeployerSubPackage(signer);
+        this.utils = new UtilsSubPackage(signer);
         
         this.contractInstance = new ContractSubPackage(signer);
         this.constants = new ConstantsSubPackage(signer);
