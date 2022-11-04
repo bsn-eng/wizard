8be10e7cf0265dfd67386d5f8b7e259caf419e42
ManulParihar <manul@blockswap.network>
2022-11-04 20:13:01 +0530

    Added _batchDepositETHForStaking function

--- logic/feesAndMevPool.js
+++ logic/feesAndMevPool.js
@@ -16,7 +16,27 @@ const _updateAccumulatedETHPerLP = async (signer, feesAndMevPoolAddress) => {
     return contract.updateAccumulatedETHPerLP();
 };
 
+const _batchDepositETHForStaking = async (signer, feesAndMevPoolAddress, blsPublicKeys, amounts, ethValue) => {
+
+    if(blsPublicKeys.length != amounts.length) {
+        throw new Error(customErrors.UNEQUAL_ARRAY_LENGTH);
+    }
+
+    for(let i=0; i<blsPublicKeys.length; ++i) {
+        blsPublicKeys[i] = _add0x(blsPublicKeys[i]);
+    }
+
+    const contract = (await getContractInstance(signer)).feesAndMevPool(feesAndMevPoolAddress);
+
+    return contract.batchDepositETHForStaking(
+        blsPublicKeys,
+        amounts,
+        { value: ethValue }
+    );
+};
+
 module.exports = {
     _totalShares,
-    _updateAccumulatedETHPerLP
+    _updateAccumulatedETHPerLP,
+    _batchDepositETHForStaking
 };
