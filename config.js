7c3cade28edb99b56e8c230c8dc6ac9e38109180
ManulParihar <manul@blockswap.network>
2022-11-06 11:12:45 +0530

    Exposed functions in FeesAndMevPoolSubPackage and SDK

--- config.js
+++ config.js
@@ -3,18 +3,21 @@ const { ConstantsSubPackage } = require('./interface/constantsClass');
 const { ContractSubPackage } = require('./interface/contractClass');
 const { UtilsSubPackage } = require('./interface/utilsClass');
 const { SavETHPoolSubPackage } = require('./interface/savETHPoolClass');
+const { FeesAndMevPoolSubPackage } = require('./interface/feesAndMevPoolClass');
 
 class Wizard {
 
     constructor({
         signer, 
         liquidStakingManagerAddress=null, 
-        savETHPoolAddress=null
+        savETHPoolAddress=null,
+        feesAndMevPoolAddress=null
     }) {
         
         this.deployer = new DeployerSubPackage(signer);
         this.utils = new UtilsSubPackage(signer, liquidStakingManagerAddress);
         this.savETHPool = new SavETHPoolSubPackage(signer, savETHPoolAddress);
+        this.feesAndMevPool = new FeesAndMevPoolSubPackage(signer, feesAndMevPoolAddress);
         
         this.contractInstance = new ContractSubPackage(signer);
         this.constants = new ConstantsSubPackage(signer);
