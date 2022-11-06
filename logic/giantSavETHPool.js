f44244abcc4413e26e808e5a44009cdd2969abdb
ManulParihar <manul@blockswap.network>
2022-11-06 11:51:17 +0530

    Added _batchRotateLPTokens function

--- logic/giantSavETHPool.js
+++ logic/giantSavETHPool.js
@@ -50,6 +50,8 @@ const _withdrawDETH = async (signer, savETHVaultAddresses, lpTokens, amounts) =>
             throw new Error(customErrors.UNEQUAL_ARRAY_LENGTH);
         }
 
+        savETHVaultAddresses[i] = _add0x(savETHVaultAddresses[i]);
+
         for(let j=0; j<lpArray.length; ++j) {
             lpTokens[i][j] = _add0x(lpTokens[i][j]);
         }
@@ -64,7 +66,42 @@ const _withdrawDETH = async (signer, savETHVaultAddresses, lpTokens, amounts) =>
     );
 };
 
+const _batchRotateLPTokens = async (signer, savETHVaultAddresses, oldLPTokens, newLPTokens, amounts) => {
+
+    const arrayLength = savETHVaultAddresses.length;
+    if(arrayLength != oldLPTokens.length || arrayLength != newLPTokens.length || arrayLength != amounts.length) {
+        throw new Error(customErrors.UNEQUAL_ARRAY_LENGTH);
+    }
+
+    for(let i=0; i<arrayLength; ++i) {
+        const oldLPArray = oldLPTokens[i];
+        const newLPArray = newLPTokens[i];
+        const amountArray = amounts[i];
+
+        if(oldLPArray.length != newLPArray.length || oldLPArray.length != amountArray.length) {
+            throw new Error(customErrors.UNEQUAL_ARRAY_LENGTH);
+        }
+
+        savETHVaultAddresses[i] = _add0x(savETHVaultAddresses[i]);
+
+        for(let j=0; j<oldLPArray.length; ++j) {
+            oldLPTokens[i][j] = _add0x(oldLPTokens[i][j]);
+            newLPTokens[i][j] = _add0x(newLPTokens[i][j]);
+        }
+    }
+
+    const contract = (await getContractInstance(signer)).giantSavETHPool();
+
+    return contract.batchRotateLPTokens(
+        savETHVaultAddresses,
+        oldLPTokens,
+        newLPTokens,
+        amounts
+    );
+};
+
 module.exports = {
     _batchDepositETHForStaking,
-    _withdrawDETH
+    _withdrawDETH,
+    _batchRotateLPTokens
 };
