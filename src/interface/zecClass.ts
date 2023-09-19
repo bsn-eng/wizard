2d641efbf4b9c97c3afb4f6a09fe8835aab07df8
ManulParihar <manul@blockswap.network>
2023-09-19 17:46:14 +0530

    Added and exposed updateDAOAddress function

--- src/interface/zecClass.ts
+++ src/interface/zecClass.ts
@@ -1,7 +1,8 @@
 import { BigNumber, Signer } from 'ethers';
 import { Provider } from '@ethersproject/abstract-provider';
 import {
-    _updateTotalZecBLSPublicKeyLimit
+    _updateTotalZecBLSPublicKeyLimit,
+    _updateDAOAddress
 } from '../logic/zec';
 
 export class ZECSubPackage {
@@ -13,5 +14,9 @@ export class ZECSubPackage {
 
     updateTotalZecBLSPublicKeyLimit(newLimit: string | BigNumber) {
         return _updateTotalZecBLSPublicKeyLimit(this.etherSigner, newLimit);
+    }
+
+    updateDAOAddress(liquidStakingManagerAddress: string, previousDAOAddress: string, newDAOAddress: string) {
+        return _updateDAOAddress(this.etherSigner, liquidStakingManagerAddress, previousDAOAddress, newDAOAddress);
     }
 }
--- src/logic/zec.ts
+++ src/logic/zec.ts
@@ -3,10 +3,22 @@ import { getContractInstance } from './contracts';
 import _ from 'lodash';
 import { BigNumber, Bytes, Signer } from 'ethers';
 import { Provider } from '@ethersproject/abstract-provider';
+import { _add0x } from './utils';
 
 export const _updateTotalZecBLSPublicKeyLimit = async (signer: Signer | Provider, newLimit: string | BigNumber) => {
 
     const contract = (await getContractInstance(signer)).zec();
 
     return contract.updateTotalZecBLSPublicKeyLimit(newLimit);
+};
+
+export const _updateDAOAddress = async (signer: Signer | Provider, liquidStakingManagerAddress: string, previousDAOAddress: string, newDAOAddress: string) => {
+
+    const contract = (await getContractInstance(signer)).zec();
+
+    return contract.updateDAOAddress(
+        _add0x(liquidStakingManagerAddress),
+        _add0x(previousDAOAddress),
+        _add0x(newDAOAddress)
+    );
 };
