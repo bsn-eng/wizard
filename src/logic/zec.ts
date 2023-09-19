c13c5c9620fcf076fb130d86922bf2cf25c4f7a7
ManulParihar <manul@blockswap.network>
2023-09-19 17:40:58 +0530

    Removed unwanted functions

--- src/logic/zec.ts
+++ src/logic/zec.ts
@@ -10,36 +10,3 @@ export const _updateTotalZecBLSPublicKeyLimit = async (signer: Signer | Provider
 
     return contract.updateTotalZecBLSPublicKeyLimit(newLimit);
 };
-
-export const _getSavETHVaultAddress = async (signer: Signer | Provider, liquidStakingManagerAddress: string) => {
-
-    const contract = (await getContractInstance(signer)).liquidStakingManager(liquidStakingManagerAddress);
-
-    return contract.savETHVault();
-};
-
-export const _add0x = (data: string) => {
-
-	if(!data) {
-		throw customErrors.NULL_OR_UNDEFINED_VALUE;
-	}
-
-	if(_.isString(data)) {
-		return (data.indexOf('0x') !== -1) ? data : '0x'.concat(data);		
-	}
-
-	return data;
-};
-
-export const _remove0x = (data: string) => {
-
-	if(!data) {
-		throw customErrors.NULL_OR_UNDEFINED_VALUE;
-	}
-
-	if(_.isString(data)) {
-		return (data.indexOf('0x') !== -1) ? data.slice(2) : data;
-	}
-
-	return data;
-};
